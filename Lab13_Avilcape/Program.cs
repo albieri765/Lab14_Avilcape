dc64acbbd890a1b15418b7e9ba3df0e37cd8571c
albieri765 <albieriaqperu@gmail.com>
2026-06-16 14:02:35 -0500

    Enable Swagger in production

--- Lab13_Avilcape/Program.cs
+++ Lab13_Avilcape/Program.cs
@@ -5,15 +5,12 @@ builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI(c =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Excel Reports API v1");
-        c.RoutePrefix = string.Empty;
-    });
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Excel Reports API v1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.MapControllers();
 app.Run();
